a9cfaab80b543cf55f327b16f7cfe82f980d4fc0
aymanhawk <ahaque2002@gmail.com>
2024-04-04 23:56:47 -0400

    LC242

--- src/main/java/NeetCode/ArraysAndHashing/ValidAnagram242.java
+++ src/main/java/NeetCode/ArraysAndHashing/ValidAnagram242.java
@@ -30,17 +30,22 @@ public class ValidAnagram242 {
     public static boolean isAnagram(String s, String t) {
         int string1Size = s.length();
         int string2Size = t.length();
+
         if (string1Size != string2Size)
             return false;
+
         ArrayList<Character> anagram1 = new ArrayList<>();
         ArrayList<Character> anagram2 = new ArrayList<>();
         for (int i = 0; i < string1Size; i++){
             anagram1.add(s.charAt(i));
             anagram2.add(t.charAt(i));
         }
+
         Collections.sort(anagram1);
         Collections.sort(anagram2);
+
         System.out.println(anagram1 + " " + anagram2);
+
         if (anagram1.equals(anagram2)) //these are not simple data types so we need to
             return true;
         else
