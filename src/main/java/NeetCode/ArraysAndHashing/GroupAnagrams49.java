59b399743b53c643bf62845f158f38ef5eaca32e
aymanhawk <ahaque2002@gmail.com>
2024-04-04 23:56:56 -0400

    LC242

---
+++ src/main/java/NeetCode/ArraysAndHashing/GroupAnagrams49.java
@@ -0,0 +1,34 @@
+package NeetCode.ArraysAndHashing;
+
+
+/*
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+Example 2:
+
+Input: strs = [""]
+Output: [[""]]
+Example 3:
+
+Input: strs = ["a"]
+Output: [["a"]]
+* */
+
+import java.util.List;
+
+public class GroupAnagrams49 {
+    public static void main(String[] args){
+        System.out.println();
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+//        List<List<String>> answer = new List<List<String>>();
+        return null;
+    }
+
+}
