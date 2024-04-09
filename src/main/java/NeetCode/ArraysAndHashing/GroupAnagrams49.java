e2b97cf007e60a73a5415e03d7a09f239eafdf2b
aymanhawk <ahaque2002@gmail.com>
2024-04-09 18:19:22 -0400

    Group Anagrams 49

--- src/main/java/NeetCode/ArraysAndHashing/GroupAnagrams49.java
+++ src/main/java/NeetCode/ArraysAndHashing/GroupAnagrams49.java
@@ -19,17 +19,34 @@ Input: strs = ["a"]
 Output: [["a"]]
 * */
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class GroupAnagrams49 {
     public static void main(String[] args){
-        System.out.println();
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(groupAnagrams(strs));
     }
 
-    public List<List<String>> groupAnagrams(String[] strs) {
-//        List<List<String>> answer = new List<List<String>>();
+    public static List<List<String>> groupAnagrams(String[] strs) {
 
-        return null;
+        HashMap<String, List<String>> group = new HashMap<>();
+
+        for (String word : strs){
+            char[] charList = word.toCharArray();
+            Arrays.sort(charList);
+            String sortedWord = new String(charList);
+
+            if (!group.containsKey(sortedWord)){
+                group.put(sortedWord, new ArrayList<>());
+            }
+
+            group.get(sortedWord).add(word);
+        }
+
+        return new ArrayList<>(group.values());
     }
 
 }
